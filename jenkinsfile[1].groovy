948bb84a4268fc61d940521cbcc8770c18e33d99
aquib khan <kaquib279@gmail.com>
2025-10-20 22:07:02 +0530

    first commit

---
+++ jenkinsfile[1].groovy
@@ -0,0 +1,12 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                // Fetch the repository using SSH credentials
+                git branch: 'main', url: 'https://github.com/khannaquib/ironpulse'
+            }
+        }
+    }        
+}
