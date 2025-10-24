2007eb72c5f6c7395096740eec4d2ed8060370a0
aquib khan <kaquib279@gmail.com>
2025-10-24 22:05:20 +0530

    added new steps to connect with nginx

--- jenkinsfile[1].groovy
+++ jenkinsfile[1].groovy
@@ -8,5 +8,12 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/khannaquib/ironpulse'
             }
         }
+        stage('testing'){
+            steps{
+                sshagent(['nginx-server']) {
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@3.25.227.47 "ls"'
+                    }
+            }
+        }
     }        
 }
